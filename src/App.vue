d229b664847d76d600be6019b7aac099e2907d2c
Elisa Michelet <43448181+arobaselisa@users.noreply.github.com>
2024-11-05 13:39:45 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
-  console.log('gwsrdgfb')
+  console.log('gwppprdgfb')
 </script>
 
 <template>
